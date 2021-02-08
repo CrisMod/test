163ec7991fd2514390541a2e19156cbdf08248aa
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-02-07 20:19:47 -0500

    Add files via upload

---
+++ mproWhatsApp.js
@@ -0,0 +1,18 @@
+{
+	"version": "13.2",
+	"download": "https://crismod.ml/mpro.html",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-pt.html"
+		}
+	]
+}
